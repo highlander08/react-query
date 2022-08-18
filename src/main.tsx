9f57db2d182440e80c285b8dcd12e6bf710de5ce
Highlander Santos <38596921+highlander08@users.noreply.github.com>
2022-08-18 13:02:28 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AppWithReactQuery from "./AppWithReactQuery";
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+defaultOptions: {
+  queries: {
+  staleTime: 86400 * 7
+  }
+}
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
